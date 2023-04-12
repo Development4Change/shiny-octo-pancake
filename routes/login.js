061bb31745d29185399001f65d0c04d74905dcbd
Denver Stewart <denverstewart4232@yahoo.com>
2023-04-12 02:09:20 -0400

    Authenication Update

--- routes/login.js
+++ routes/login.js
@@ -1,19 +1,32 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/users')
+//const jwt = require('jsonwebtoken');
+//const { JsonWebTokenError } = require('jsonwebtoken')
+//const email = req.body.email
+//const password = req.body.password
+//var session
 
 
 router.get('/', (req, res) => {
-    console.log('Successful login launch')
+    session=req.session;
+    if(session.userid){
+        res.send("index");
+    }else
     res.render('login');
+    console.log('Successful login launch')
+   
 })
 
 router.post('/', async (req, res) => {
- 
+ //Authenticate User
    try{
         const check = await User.findOne({ email: req.body.email })
 
         if(check.password === req.body.password){
+            session=req.session;
+            session.userid=req.body.email;
+            console.log(req.session)
             res.render('index')
         }
         else{
@@ -23,6 +36,17 @@ router.post('/', async (req, res) => {
         console.log(e)
         res.send('Incorrect Email or Password')
     }
+   
+    /*const email = req.body.email
+    const user = {username: email}
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+    res.json({accessToken: accessToken})*/
 })
+
+router.get('/logout',(req,res) => {
+    req.session.destroy();
+    res.redirect('/login');
+    console.log('session destroyed')
+});
 
 module.exports = router
